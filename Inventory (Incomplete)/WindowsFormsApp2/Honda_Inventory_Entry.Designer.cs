bf614db056d68286202aa0c0fc0d2e890694562b
bossetali <bossetali@gmail.com>
2022-08-25 11:11:35 -0700

    Add files via upload

---
+++ Inventory (Incomplete)/WindowsFormsApp2/Honda_Inventory_Entry.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Honda_Inventory_Entry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.listBox1);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.listBox2);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.listBox3);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.listBox4);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(345, 897);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Select a Year";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2011",
+            "2010",
+            "2009",
+            "2008",
+            "2007",
+            "2006",
+            "2005",
+            "2004",
+            "2003",
+            "2002",
+            "2001",
+            "2000",
+            "1999",
+            "1998",
+            "1997",
+            "1996",
+            "1995",
+            "1994",
+            "1993",
+            "1992",
+            "1991",
+            "1990"});
+            this.listBox1.Location = new System.Drawing.Point(3, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 129);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please Select a Model";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(3, 188);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(306, 179);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please Select a Sub-Model";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(3, 398);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(306, 229);
+            this.listBox3.TabIndex = 5;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 630);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Please Select an Engine";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 25;
+            this.listBox4.Location = new System.Drawing.Point(3, 658);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(306, 204);
+            this.listBox4.TabIndex = 7;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1132, 766);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 103);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Output to Txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Honda_Inventory_Entry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 897);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Honda_Inventory_Entry";
+            this.Text = "Honda_Inventory_Entry";
+            this.Load += new System.EventHandler(this.Honda_Inventory_Entry_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Inventory (Incomplete)/WindowsFormsApp2/Honda_Inventory_Entry.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Honda_Inventory_Entry : Form
+    {
+        ListBox listbox5;
+        public Honda_Inventory_Entry()
+        {
+            InitializeComponent();
+            listBox1.SelectionMode = SelectionMode.One;
+            listBox2.SelectionMode = SelectionMode.One;
+            listbox5 = new ListBox();
+        }
+
+        private void Honda_Inventory_Entry_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            string curItem2 = listBox1.SelectedItem.ToString();
+            String[] Models = new string[100];
+            switch (curItem2)
+            {
+                case "2019":
+                    Models = new String[] { "Accord","Civic" };
+                    break;
+                case "2018":
+                    Models = new String[] { "This","That" };
+                    break;
+                case "2017":
+                    Models = new String[] { "this","That" };
+                    break;
+                case "2016":
+                    Models = new String[] { "this","That" };
+                    break;
+                case "2015":
+                    Models = new String[] { "this","That" };
+                    break;
+                case "2014":
+                    Models = new String[] { "this","That" };
+                    break;
+                case "2013":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2012":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2011":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2010":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2009":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2008":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2007":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2006":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2005":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2004":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2003":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2002":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2001":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "2000":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1999":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1998":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1997":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1996":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1995":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1994":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1993":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1992":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1991":
+                    Models = new String[] { "this", "That" };
+                    break;
+                case "1990":
+                    Models = new String[] { "this", "That" };
+                    break;
+            }
+            foreach (String i in Models)
+            {
+                listBox2.Items.Add(i);
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            string curItem2 = listBox2.SelectedItem.ToString();
+            String[] Models = new string[100];
+            switch (curItem2)
+            {
+                case "Accord":
+                    Models = new String[] { "EX", "LX" };
+                    break;
+                case "Civic":
+                    Models = new String[] { "EX", "LX" };
+                    break;
+            }
+            foreach (String i in Models)
+            {
+                listBox3.Items.Add(i);
+            }
+
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+            string curItem2 = listBox3.SelectedItem.ToString();
+            String[] Models = new string[100];
+            switch (curItem2)
+            {
+                case "EX":
+                    Models = new String[] { "L4-1.5", "L4-2.0" };
+                    break;
+                case "LX":
+                    Models = new String[] { "L4-1.5", "L4-2.0" };
+                    break;
+            }
+            foreach (String i in Models)
+            {
+                listBox4.Items.Add(i);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            string curItem1 = listBox1.SelectedItem.ToString();
+            string curItem2 = listBox2.SelectedItem.ToString();
+            string curItem3 = listBox3.SelectedItem.ToString();
+            string curItem4 = listBox4.SelectedItem.ToString();
+            listbox5.Items.Add(curItem1);
+            listbox5.Items.Add(curItem2);
+            listbox5.Items.Add(curItem3);
+            listbox5.Items.Add(curItem4);
+
+            //Code for the Parts 
+
+
+            string filename = string.Join( curItem1, curItem2, curItem3,curItem4 );
+
+            string sPath = "C:\\Users\\korin\\Documents\\Inventory\\Honda\\"+filename+".txt";
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listbox5.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+            MessageBox.Show("Inventory saved!");
+
+            this.Close();
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
